48970920deaa622cdfce9021dce66252a159388a
Weronika Górska <weronika.oliwia.gorska@gmail.com>
2024-12-10 21:16:11 +0100

    Added required constraints to relationships.

--- ContractManagementSystem.Core/ContractManagementDbContext.cs
+++ ContractManagementSystem.Core/ContractManagementDbContext.cs
@@ -17,25 +17,29 @@ namespace ContractManagementSystem.Core
             modelBuilder.Entity<Contract>()
                 .HasOne(c => c.Category)
                 .WithOne(cat => cat.Contract)
-                .HasForeignKey<Contract>(c => c.CategoryId);
+                .HasForeignKey<Contract>(c => c.CategoryId)
+                .IsRequired();
 
             // Configure One-to-Many relationship between Contract and Addendums
             modelBuilder.Entity<Addendum>()
                 .HasOne(a => a.Contract)
                 .WithMany(c => c.Addendums)
-                .HasForeignKey(a => a.ContractId);
+                .HasForeignKey(a => a.ContractId)
+                .IsRequired();
 
             // Configure One-to-Many relationship between Addendum and Product Items
             modelBuilder.Entity<ProductItem>()
                 .HasOne(p => p.Addendum)
                 .WithMany(a => a.ProductItems)
-                .HasForeignKey(p => p.AddendumId);
+                .HasForeignKey(p => p.AddendumId)
+                .IsRequired();
 
             // Configure One-to-Many relationship between Addendum and Responsible Persons
             modelBuilder.Entity<ResponsiblePerson>()
                 .HasOne(rp => rp.Addendum)
                 .WithMany(a => a.ResponsiblePersons)
-                .HasForeignKey(rp => rp.AddendumId);
+                .HasForeignKey(rp => rp.AddendumId)
+                .IsRequired();
         }
     }
 }
