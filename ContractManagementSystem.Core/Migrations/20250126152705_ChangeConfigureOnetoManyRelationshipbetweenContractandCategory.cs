78fd54b2f6dd658ab152020a045d77bddc7e4f65
Weronika Górska <weronika.oliwia.gorska@gmail.com>
2025-01-26 16:27:50 +0100

    Fix Configure One to Many Relationship between Contract and Category

---
+++ ContractManagementSystem.Core/Migrations/20250126152705_ChangeConfigureOnetoManyRelationshipbetweenContractandCategory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContractManagementSystem.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeConfigureOnetoManyRelationshipbetweenContractandCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Contracts_CategoryId",
+                table: "Contracts");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contracts_CategoryId",
+                table: "Contracts",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Contracts_CategoryId",
+                table: "Contracts");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contracts_CategoryId",
+                table: "Contracts",
+                column: "CategoryId",
+                unique: true);
+        }
+    }
+}
